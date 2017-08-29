dbe887fe32a9cd5989b56489a763fa5768c0fc5b
weiqiang <weiqiang@huobi.com>
2017-08-29 15:16:45 +0800

    Before making moving object along the path

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -8,7 +10,247 @@ import priceCard from '../src/components/priceCard.vue';
 
 Vue.config.productionTip = true
 
+let debug = true;
+
 /* eslint-disable no-new */
+
+function LineChart_1(options){
+  // user defined properties
+  this.canvas = document.querySelector(options.canvasId);
+  this.minX = options.minX;
+  this.minY = options.minY;
+  this.maxX = options.maxX;
+  this.maxY = options.maxY;
+  this.unitsPerTickX = options.unitsPerTickX;
+  this.unitsPerTickY = options.unitsPerTickY;
+
+  // constants
+  this.padding = 10;
+  this.tickSize = 10;
+  this.axisColor = '#555';
+  this.pointRadius = 5;
+  this.fontHeight = 12;
+
+  // relationships
+  this.context = this.canvas.getContext('2d');
+  this.rangeX = this.maxX - this.minX;
+  this.rangeY = this.maxY - this.minY;
+  this.numXTicks = Math.round(this.rangeX / this.unitsPerTickX);
+  this.numYTicks = Math.round(this.rangeY / this.unitsPerTickY);
+  this.x = this.getLongestValueWidth() + this.padding * 2;
+  this.y = this.padding * 2;
+  this.width = this.canvas.width - this.x - this.padding * 2;
+  this.height = this.canvas.height - this.y - this.padding - this.fontHeight;
+  this.scaleX = this.width / this.rangeX || 1;
+  this.scaleY = this.height / this.rangeY || 1;
+  console.log('??? LineChart_1 maxX, minX, rangeX, width: ', this.maxX, this.minX, this.rangeX, this.width);
+
+  // draw x y axis and tick marks
+  this.drawXAxis();
+  this.drawYAxis();
+
+}
+
+LineChart_1.prototype.getLongestValueWidth = function () {
+  let longestValueWidth = 0;
+  for (let n=0; n<=this.numYTicks; n++) {
+    let value = this.maxY - (n*this.unitsPerTickY);
+    longestValueWidth = Math.max(longestValueWidth, this.context.measureText(value).width);
+  }
+  return longestValueWidth;
+};
+
+LineChart_1.prototype.drawXAxis = function () {
+  let context = this.context;
+
+  context.save();
+  context.beginPath();
+  context.moveTo(this.x, this.y + this.height);
+  context.lineTo(this.x + this.width, this.y + this.height);
+  context.closePath();
+  context.strokeStyle = this.axisColor;
+  context.lineWidth = 2;
+  context.stroke();
+
+  // draw tick marks
+  for (let n=0; n<this.numXTicks; n++){
+    context.beginPath();
+    context.moveTo((n+1)*this.width/this.numXTicks+this.x, this.y+this.height);
+    context.lineTo((n+1)*this.width/this.numXTicks+this.x, this.y+this.height-this.tickSize);
+    context.closePath();
+    context.stroke();
+  }
+
+  // draw labels
+  context.fillStyle = 'black';
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+
+  for (let n=0; n<this.numXTicks; n++) {
+    let label = Math.round((n+1)*this.maxX/this.numXTicks);
+    context.save();
+    context.translate((n+1)*this.width/this.numXTicks+this.x, this.y+this.height+this.padding);
+    context.fillText(label, 0, 0);
+    context.restore();
+  }
+  context.restore();
+}
+
+LineChart_1.prototype.drawYAxis = function () {
+  let context = this.context;
+  context.save();
+
+  context.save();
+  context.beginPath();
+  context.moveTo(this.x, this.y);
+  context.lineTo(this.x, this.y + this.height);
+  context.closePath();
+  context.strokeStyle = this.axisColor;
+  context.lineWidth = 2;
+  context.stroke();
+  context.restore();
+
+  // draw tick marks
+  for (let n=0; n<this.numYTicks; n++) {
+    context.beginPath();
+    context.moveTo(this.x, n*this.height/this.numYTicks+this.y);
+    context.lineTo(this.x+this.tickSize, n*this.height/this.numYTicks+this.y);
+    context.closePath();
+    context.stroke();
+  }
+
+  // draw values
+  context.fillStyle = 'black';
+  context.textAlign = 'right';
+  context.textBaseline = 'middle';
+
+  for (let n=0; n<this.numYTicks; n++) {
+    let value = Math.round(this.maxY - n*this.maxY/this.numYTicks);
+
+    context.save();
+    context.translate(this.x-this.padding, n*this.height/this.numYTicks+this.y);
+    context.fillText(value, 0, 0);
+    context.restore();
+  }
+  context.restore();
+};
+
+LineChart_1.prototype.drawLine = function(data, color, width) {
+  let context = this.context;
+
+  context.save();
+  this.transformContext();
+  context.lineWidth = width;
+  context.strokeStyle = color;
+  context.fillStyle = color;
+  context.beginPath();
+  context.moveTo(data[0].x * this.scaleX, 0 * this.scaleY);
+
+  for (let n=0; n<data.length; n++) {
+    let point = data[n];
+
+    // draw segment
+    context.lineTo(point.x*this.scaleX, point.y*this.scaleY);
+    context.stroke();
+
+    context.save();
+    context.beginPath();
+    context.arc(point.x*this.scaleX, point.y*this.scaleY,this.pointRadius, 0, 2*Math.PI, false);
+    context.fill();
+    context.closePath();
+    context.restore();
+
+    // position for next segment
+    context.moveTo(point.x*this.scaleX, point.y*this.scaleY);
+  }
+  context.lineTo(data[data.length-1].x*this.scaleX, 0*this.scaleY);
+  context.lineTo(data[0].x*this.scaleX, data[0]*this.scaleY);
+  console.log('??? drawLine data: ', data[data.length-1]);
+  context.closePath();
+  context.fill();
+  context.restore();
+};
+
+LineChart_1.prototype.transformContext = function () {
+  let context = this.context;
+
+  // move context to center of canvas
+  context.translate(this.x, this.y + this.height);
+
+  // invert the y scale so that that increments 
+  // as you move upwards
+  context.scale(1, -1);
+};
+
+var app31 = new Vue({
+  el: '#app31',
+  mounted () {
+    let options = {
+      canvasId: '#myCanvas',
+      minX: 0,
+      minY: 0,
+      maxX: 140,
+      maxY: 100,
+      unitsPerTickX: 10,
+      unitsPerTickY: 10
+    };
+    let lineChart = new LineChart_1(options);
+    let data = [{
+      x: 0,
+      y: 0
+    }, {
+      x: 20,
+      y: 10
+    }, {
+      x: 40,
+      y: 15
+    }, {
+      x: 60,
+      y: 40
+    }, {
+      x: 80,
+      y: 60
+    }, {
+      x: 100,
+      y: 50
+    }, {
+      x: 120,
+      y: 85
+    }, {
+      x: 140,
+      y: 100
+    }];
+
+    lineChart.drawLine(data, 'blue', 3);
+
+    data = [{
+      x: 20,
+      y: 85
+    }, {
+      x: 40,
+      y: 75
+    }, {
+      x: 60,
+      y: 75
+    }, {
+      x: 80,
+      y: 45
+    }, {
+      x: 100,
+      y: 65
+    }, {
+      x: 120,
+      y: 40
+    }, {
+      x: 140,
+      y: 35
+    }];
+
+    //lineChart.drawLine(data, 'red', 3);
+  }
+});
+
+
 var app1 = new Vue({
   el: '#app1',
   data: {
@@ -407,10 +649,8 @@ var app15 = new Vue({
   },
   methods: {
     doThis(){
-      console.log('??? Coming from event a.');
     },
     doThat(){
-      console.log('??? Coming from event b.');
     }
   },
 });
@@ -703,3 +943,409 @@ var app25 = new Vue({
   }
 });
 
+var app26 = new Vue({
+  el: '#app26',
+  data: {
+    dataObj: {
+      a: 1
+    }
+  },
+  computed: {
+    dataObjClone: function(){
+      //return JSON.parse(JSON.stringify(this.dataObj));
+      let newObj = Object.assign({}, this.dataObj);
+      return newObj;
+    }
+  },
+  watch: {
+    dataObjClone: function(newVal, oldVal){
+    }
+  }
+});
+
+window.app26 = app26;
+
+var app27 = new Vue({
+  el: '#app27',
+  data: {
+    items: [{name: 'J', age: '22'}, {name: 'R', age: '33'}]
+  },
+  watch: {
+    itemsClone: function(newVal, oldVal){
+    }
+  },
+  computed: {
+    itemsClone: function(){
+      let newItems = this.items.slice(0);
+      //return newItems;
+      return JSON.parse(JSON.stringify(this.items));
+    }
+  },
+  methods: {
+    changeAge: function(item, event){
+    }
+  },
+
+});
+
+window.app27 = app27;
+
+
+let app28 = new Vue({
+  el: '#app28',
+  data() {
+    return {
+      now: 0,
+      intervalId: 0
+    }
+  },
+  mounted() {
+    let self = this;
+    function step(ts){
+      self.now = Date.now();
+      self.intervalId = requestAnimationFrame(step);
+    }
+    requestAnimationFrame(step);
+  },
+  computed: {
+    difference () {
+      let [year, month, day, hour, minute, second] = [2017, 7, 13, 21, 45, 0];
+      let objectTime = Date.parse(new Date(year, month, day, hour, minute, second));
+      let difference = Math.floor((objectTime - this.now)/1000);
+      if(difference <= 0){
+        cancelAnimationFrame(this.intervalId);
+      }
+      return difference;
+    }
+  },
+  methods: {
+  },
+  watched: {
+  }
+});
+
+function draw () {
+  let canvas = document.querySelector('#canvas');
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+
+    ctx.fillRect(0, 0, 50, 50);
+    ctx.clearRect(10, 10, 10, 10);
+    ctx.strokeRect(10, 10, 5, 5);
+
+  }
+}
+
+function drawTriangle () {
+  let canvas = document.querySelector('#canvas');
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo( 100, 100 );
+    ctx.lineTo( 200, 100 );
+    ctx.lineTo( 100, 200 );
+    ctx.closePath();
+    ctx.fill();
+  }
+}
+
+function drawSmile () {
+  let canvas = document.querySelector('#canvas');
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+
+    ctx.arc(50, 50, 50, 0, Math.PI * 2, true);
+    ctx.moveTo(25, 25);
+    ctx.arc(25, 25, 10, 0, Math.PI * 2, true);
+    ctx.moveTo(75, 25);
+    ctx.arc(75, 25, 10, 0, Math.PI * 2, true);
+    ctx.moveTo(50, 55);
+    ctx.arc(50, 55, 30, Math.PI * 1, Math.PI * 2, true);
+    ctx.stroke();
+  }
+}
+
+function drawTwoTriangles () {
+  let canvas = document.querySelector('#canvas');
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo(25, 25);
+    ctx.lineTo(75, 25);
+    ctx.lineTo(25, 75);
+    ctx.lineTo(25, 25);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(25, 75);
+    ctx.lineTo(75, 75);
+    ctx.lineTo(75, 25);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.lineTo(200, 200);
+    ctx.stroke();
+
+  }
+}
+
+function drawLoopFor () {
+  let canvas = document.querySelector('#canvas');
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+
+    for (let i=0; i<4; i++) {
+      for (let j=0; j<3; j++) {
+        ctx.beginPath();
+        let x = 25 + j * 50;
+        let y = 25 + i * 50;
+        let radius = 20;
+        let startAngle = 0;
+        let endAngle = Math.PI + (Math.PI * j) / 2;
+        let anticlockwise = i % 2 !== 0;
+
+        ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+
+        if (i > 1) {
+          ctx.fill();
+        } else {
+          ctx.stroke();
+        }
+
+      }
+    }
+  }
+}
+
+function drawPath2D () {
+  let canvas = document.querySelector('#canvas');
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'green';
+
+    let rectangle = new Path2D();
+    rectangle.rect(10, 10, 30, 30);
+    ctx.stroke(rectangle);
+
+    let circle = new Path2D();
+    circle.arc(100, 100, 50, 0, Math.PI, true);
+    ctx.stroke(circle);
+    ctx.fill(circle);
+
+    ctx.strokeStyle = 'blue';
+
+    ctx.arc(200, 200, 10, 0, Math.PI * 2, false);
+    ctx.stroke();
+
+    let p = new Path2D('M10 10 h 80 v 80 h -80 Z');
+    ctx.stroke(p);
+
+  }
+}
+
+function drawGlobalAlpha () {
+  let canvas = document.querySelector('#canvas');
+  if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = '#fd0';
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.globalAlpha = 0.2;
+
+    for (let i = 0; i < 7; i++) {
+      ctx.beginPath();
+      ctx.arc(60, 60, 10 + 10 * i, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }
+
+
+}
+
+var app29 = new Vue({
+  el: '#app29',
+  mounted() {
+    //draw();
+    //drawTriangle();
+    //drawSmile();
+    //drawTwoTriangles();
+    //drawLoopFor();
+    //drawPath2D();
+    drawGlobalAlpha();
+  }
+});
+
+
+let data = [
+  {
+    x: 120,
+    y: 48
+  },
+  {
+    x: 240,
+    y: 96
+  },
+  {
+    x: 360,
+    y: 144
+  },
+  {
+    x: 480,
+    y: 192
+  },
+  {
+    x: 840,
+    y: 336
+  },
+  {
+    x: 1100,
+    y: 30
+  },
+];
+
+function drawDepthChart () {
+  let canvas = document.querySelector('#depthChart');
+  let ctx = canvas.getContext('2d');
+  let options = {
+    canvasId: '#depthChart',
+    maxX: 1200,
+    minX: 0,
+    maxY: 480,
+    minY: 0,
+    unitsPerTickX: 120,
+    unitsPerTickY: 48,
+  }
+  let fillColor = 'LightCoral';
+
+  let lineChart = new LineChart(options);
+  lineChart.drawXAxis();
+  lineChart.drawYAxis();
+  lineChart.drawLine(data, fillColor);
+}
+
+function LineChart (options) {
+  // user defined properties
+  this.canvas = document.querySelector(options.canvasId);
+  this.maxX = options.maxX;
+  this.minX = options.minX;
+  this.maxY = options.maxY;
+  this.minY = options.minY;
+  this.unitsPerTickX = options.unitsPerTickX;
+  this.unitsPerTickY = options.unitsPerTickY;
+
+  // constants
+  this.padding = 10;
+  this.axisColor = '#555';
+  this.tickSize = 10;
+  this.fontHeight = 12;
+
+  // relationships
+  this.context = this.canvas.getContext('2d');
+  this.rangeY = this.maxY - this.minY;
+  this.rangeX = this.maxX - this.minX;
+  this.numXTicks = Math.round(this.rangeX / this.unitsPerTickX);
+  this.numYTicks = Math.round(this.rangeY / this.unitsPerTickY);
+  this.x = this.getLongestValueWidth() + this.padding * 2;
+  this.y = this.padding * 2;
+  this.width = this.canvas.width - this.x - this.padding * 2;
+  this.height = this.canvas.height - this.y - this.padding - this.fontHeight;
+}
+
+LineChart.prototype.getLongestValueWidth = function () {
+  let longestValueWidth = 0;
+  for (let n = 0; n <= this.numYTicks; n++) {
+    let value = this.maxY - (n * this.unitsPerTickY);
+    let width = this.context.measureText(value).width;
+    longestValueWidth = Math.max(longestValueWidth, this.context.measureText(value).width);
+  }
+  return longestValueWidth;
+};
+
+LineChart.prototype.drawXAxis = function () {
+  let context = this.context;
+  context.save();
+
+  // draw the xAxis
+  context.beginPath();
+  context.moveTo(this.x, this.y + this.height);
+  context.lineTo(this.x + this.width, this.y + this.height);
+  context.strokeStyle = this.axisColor;
+  context.lineWidth = 2;
+  context.stroke();
+
+  // draw tick marks
+  for (let n = 0; n < this.numXTicks; n++) {
+    context.beginPath();
+    context.moveTo((n+1)*this.width/this.numXTicks + this.x, this.y + this.height);
+    context.lineTo((n+1)*this.width/this.numXTicks + this.x, this.y + this.height-this.tickSize);
+    context.stroke();
+  }
+
+  // draw labels
+  context.fillStyle = 'black';
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  for (let n=0; n<this.numXTicks; n++){
+    let label = Math.round((n+1)*this.maxX/this.numXTicks);
+    context.save();
+    context.translate((n+1)*this.width/this.numXTicks + this.x, this.y + this.height + this.padding);
+    context.fillText(label, 0, 0);
+    context.restore();
+  }
+  context.restore();
+  
+};
+
+LineChart.prototype.drawYAxis = function () {
+  let {context, x, y, height, numYTicks, tickSize, fontHeight, maxY } = this;
+
+  // draw y axis
+  context.moveTo(x, y);
+  context.lineTo(x, y + height);
+  context.stroke();
+
+  // draw tick marks
+  for (let n=0; n<numYTicks; n++) {
+    context.moveTo(x, (n) * (height/numYTicks) + y);
+    context.lineTo(x+tickSize, (n) * (height/numYTicks) + y);
+    context.stroke();
+  }
+
+  // draw labels
+  context.fillStyle = 'black';
+  context.textAlign = 'left';
+  context.textBaseline = 'middle';
+  for (let n=0; n<numYTicks; n++) {
+    let label = Math.round((numYTicks-n)*(maxY/numYTicks));
+    context.fillText(label, x-tickSize-fontHeight, (n) * (height/numYTicks) + y);
+  }
+}
+
+LineChart.prototype.drawLine = function (data, fillColor) {
+  let {context, y} = this;
+  let length = data.length;
+  this.transformContext();
+  context.moveTo(data[0].x, 0);
+  for (let d of data) {
+    context.lineTo(d.x, d.y);
+  }
+  context.lineTo(data[length-1].x, 0)
+  context.fillStyle = fillColor;
+  context.fill();
+};
+
+LineChart.prototype.transformContext = function () {
+  let {context} = this;
+  context.translate(this.x, this.y + this.height);
+  context.scale(1, -1);
+}
+
+var app30 = new Vue({
+  el: '#app30',
+  mounted () {
+    drawDepthChart();
+  }
+});
